1728af6d10eb3e62b8dc1bc65398bd226449a5db
exsp <18811798328@163.com>
2018-02-07 17:00:18 +0800

    clean

--- mesh-seg/Util.h
+++
@@ -1,5 +0,0 @@
-#ifndef _UTIL_H_
-#define _UTIL_H_
-const double eps = 1e-8;
-int sign(double x);
-#endif
--- mesh-seg/gy_show.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "lty_STL.h"
-void InitMesh(Mesh& mdata);		//初始化网格
-//void drawSTL(Mesh &mesh);		//画模型
